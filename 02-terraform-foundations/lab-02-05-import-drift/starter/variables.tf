8f5eb47c7ced2b08409e8ea8805ea81af094806e
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 23:31:54 +0100

    labs: complete Course 02 starter

--- 02-terraform-foundations/lab-02-05-import-drift/starter/variables.tf
+++ 02-terraform-foundations/lab-02-05-import-drift/starter/variables.tf
@@ -1,23 +1,7 @@
-variable "subscription_id" {
-  type     = string
-  nullable = false
-}
-
-variable "resource_group_name" {
-  type     = string
-  nullable = false
-}
-
-variable "location" {
-  type    = string
-  default = "westeurope"
-}
-
+variable "subscription_id" { type = string nullable = false }
+variable "resource_group_name" { type = string nullable = false }
+variable "location" { type = string default = "westeurope" }
 variable "tags" {
   type = map(string)
-  default = {
-    academy = "data2ai"
-    course  = "terraform-foundations"
-    lab     = "02-05"
-  }
+  default = { academy = "data2ai", course = "terraform-foundations", lab = "02-05" }
 }

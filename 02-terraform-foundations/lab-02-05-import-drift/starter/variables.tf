1a6570fea8108ce6d0dac0539c9200904a2daa45
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 23:31:37 +0100

    labs: complete Course 02 executable validation

---
+++ 02-terraform-foundations/lab-02-05-import-drift/starter/variables.tf
@@ -0,0 +1,23 @@
+variable "subscription_id" {
+  type     = string
+  nullable = false
+}
+
+variable "resource_group_name" {
+  type     = string
+  nullable = false
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    academy = "data2ai"
+    course  = "terraform-foundations"
+    lab     = "02-05"
+  }
+}

27ee76536bbe71c5a75b06827179731e4379e2d0
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 23:31:52 +0100

    labs: complete Course 02 starter

--- 02-terraform-foundations/lab-02-05-import-drift/starter/main.tf
+++ 02-terraform-foundations/lab-02-05-import-drift/starter/main.tf
@@ -1,28 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
+  required_providers { azurerm = { source = "hashicorp/azurerm", version = "~> 4.0" } }
 }
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
-
+provider "azurerm" { features {} subscription_id = var.subscription_id }
 resource "azurerm_resource_group" "imported" {
-  name     = var.resource_group_name
+  name = var.resource_group_name
   location = var.location
-  tags     = var.tags
+  tags = var.tags
 }
-
-output "resource_group_id" {
-  value = azurerm_resource_group.imported.id
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.imported.name
-}
+output "resource_group_id" { value = azurerm_resource_group.imported.id }
+output "resource_group_name" { value = azurerm_resource_group.imported.name }

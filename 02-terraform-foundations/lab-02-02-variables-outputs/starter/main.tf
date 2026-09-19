1277604144d21e3a44f1afadb647b4d7fdfc7efb
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 23:30:36 +0100

    labs: implement starter

---
+++ 02-terraform-foundations/lab-02-02-variables-outputs/starter/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "lab" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}

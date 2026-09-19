a44fbd32c8cfe8b81426c5f53b3ab08836acc568
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 23:31:00 +0100

    labs: implement starter

--- 02-terraform-foundations/lab-02-02-variables-outputs/starter/outputs.tf
+++ 02-terraform-foundations/lab-02-02-variables-outputs/starter/outputs.tf
@@ -1,19 +1,15 @@
 output "resource_group_id" {
-  description = "Resource Group resource ID."
-  value       = azurerm_resource_group.lab.id
+  value = azurerm_resource_group.lab.id
 }
 
 output "resource_group_name" {
-  description = "Resource Group name."
-  value       = azurerm_resource_group.lab.name
+  value = azurerm_resource_group.lab.name
 }
 
 output "resource_group_location" {
-  description = "Resource Group location."
-  value       = azurerm_resource_group.lab.location
+  value = azurerm_resource_group.lab.location
 }
 
 output "resource_group_tags" {
-  description = "Resource Group tags."
-  value       = azurerm_resource_group.lab.tags
+  value = azurerm_resource_group.lab.tags
 }

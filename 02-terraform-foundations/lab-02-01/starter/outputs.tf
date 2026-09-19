517ef758136570bc515bcbaf03be10e6265ef153
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 13:10:50 +0100

    cleanup: remove duplicate lab 02-01

--- 02-terraform-foundations/lab-02-01/starter/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "resource_group_id" {
-  description = "Resource group resource ID."
-  value       = azurerm_resource_group.lab.id
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.lab.name
-}

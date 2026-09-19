c43fd8aaa1b98ea08af403d5e14e3517b0e1c493
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 23:31:47 +0100

    labs: complete Course 02 starter

--- 02-terraform-foundations/lab-02-04-state/starter/outputs.tf
+++ 02-terraform-foundations/lab-02-04-state/starter/outputs.tf
@@ -1,14 +1,3 @@
-output "resource_group_id" {
-  value = azurerm_resource_group.lab.id
-}
-
-output "storage_account_id" {
-  value = azurerm_storage_account.state_demo.id
-}
-
-output "state_addresses" {
-  value = [
-    "azurerm_resource_group.lab",
-    "azurerm_storage_account.state_demo"
-  ]
-}
+output "resource_group_id" { value = azurerm_resource_group.lab.id }
+output "storage_account_id" { value = azurerm_storage_account.state_demo.id }
+output "state_addresses" { value = ["azurerm_resource_group.lab", "azurerm_storage_account.state_demo"] }

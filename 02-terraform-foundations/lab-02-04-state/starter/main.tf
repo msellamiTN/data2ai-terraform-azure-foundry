29a4680beb4b654aa17f60dbeddaa41374fc6495
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-19 23:31:17 +0100

    labs: implement starter

---
+++ 02-terraform-foundations/lab-02-04-state/starter/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "lab" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_storage_account" "state_demo" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.lab.name
+  location                 = azurerm_resource_group.lab.location
+  account_tier              = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+  tags                     = var.tags
+}

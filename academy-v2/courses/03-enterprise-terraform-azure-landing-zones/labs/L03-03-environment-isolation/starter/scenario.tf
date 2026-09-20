0e9b1da066c97800fa44eaa3f9c244cea5b1bf25
msellamiTN <mokhtar.sellami@gmail.com>
2026-09-20 16:31:48 +0100

    feat(lab): add executable Terraform scenario for L03-03-environment-isolation

---
+++ academy-v2/courses/03-enterprise-terraform-azure-landing-zones/labs/L03-03-environment-isolation/starter/scenario.tf
@@ -0,0 +1,3 @@
+# Executable Terraform scenario for L03-03-environment-isolation
+resource "azurerm_cognitive_account" "ai" { name=replace("L03-03-environment-isolationai","-","") location=var.location resource_group_name=azurerm_resource_group.this.name kind="AIServices" sku_name="S0" custom_subdomain_name=replace("L03-03-environment-isolationai","-","") identity { type="SystemAssigned" } tags=local.tags }
+resource "azurerm_storage_account" "knowledge" { name=replace("L03-03-environment-isolationst","-","") resource_group_name=azurerm_resource_group.this.name location=var.location account_tier="Standard" account_replication_type="LRS" min_tls_version="TLS1_2" tags=local.tags }
